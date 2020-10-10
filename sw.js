3517e2491aff8ae725d04a56b815a9c7120540be
Ferdiansyah <ferdif9996@gmail.com>
2020-10-11 06:39:00 +0700

    Integrate bootstrap

--- sw.js
+++ sw.js
@@ -2,9 +2,10 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        './src/routing.js',
+        /*'./src/routing.js',*/
         './example/lavosted.min.js',
-        'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css',
+        './public/css/bootstrap.min.css',
+        './public/js/bootstrap.bundle.min.js',
       ]);
     })
   );
